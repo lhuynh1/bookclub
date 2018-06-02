0f2bf232dec4086a87941ec0af503b257cdcf63e
hkichen <hkichen@gmail.com>
2018-06-02 11:23:20 -0400

    resolved all conflicts from merging posts to dev

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-// Dependencies 
-var express = require("express");
-var bodyParser = require("body-parser");
-var passport = require('passport');
-
-// express app
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-// require models
-var db = require("./models");
-
-// Set Handlebars
-var exphbs = require("express-handlebars");
-=======
 var express = require("express");
 var bodyParser = require("body-parser");
 var db = require("./models")
@@ -23,32 +7,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
->>>>>>> posts
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-<<<<<<< HEAD
-// Static directory
-app.use(express.static(__dirname + '/public'));
-
-// handling data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-// routes
-require("./routes/discussion-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
-require("./routes/html-routes.js")(app);
-
-// sync sequelize models & start express app
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function () {
-        console.log(`App listenting on PORT: ${PORT}`);
-    });
-=======
 app.use(express.static(__dirname + "/public"));
 
 //require defined routes
@@ -61,5 +23,4 @@ app.get("*", (req, res) => res.status(200).send({
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
->>>>>>> posts
 });
